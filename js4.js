0fff0a3f8c39254539a8d19895ef20c46a7466f3
Chiheb13 <Yahyachiheb8@gmail.com>
2023-11-23 00:47:14 -1200

    first commit

---
+++ js4.js
@@ -0,0 +1,9 @@
+function charge(){
+  document.bebe.src="../images/bebe/b1.gif";
+  //document.getElementById("enfant").src="../images/b1.gif";
+
+}
+function decharger(){
+    document.bebe.src="../images/bebe/a1.gif";
+    //document.getElementById("enfant").src="../images/b9.gif";
+}
